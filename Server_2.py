87fef55b30ab9b072044114142b518cf2731a245
Javith farkhan. S <94296805+Javith-farkhan@users.noreply.github.com>
2023-05-28 19:05:26 +0530

    Add files via upload

---
+++ Server_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket
+s=socket.socket()
+s.connect(('localhost',8000))
+while True:
+	print(s.recv(1024).decode())
+	s.send("acknowledgement recieved from the server".encode())
+
+
+# In[ ]:
+
+
+
+
